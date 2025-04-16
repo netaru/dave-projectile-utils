34ad7d4d77490f0bd940cc9607cb4836f490a8f9
David Jonsson <david.jonsson306@gmail.com>
2025-04-16 18:09:07 +0200

    Added a new file for lsp
    Want to add a lsp-workspace-symbols wrapper with completing-read

--- dave-projectile-utils.el
+++ dave-projectile-utils.el
@@ -20,7 +20,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 ;;; Commentary:
 
 ;; Just a lazy convenience package for me.
